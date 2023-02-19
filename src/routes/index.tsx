d0f302e656a772400db8657627b1e68fcdf07f18
Ian Brown <ian.brown@spri.ng>
2023-02-19 00:27:51 -0700

    adding in server info

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,54 +1,60 @@
+import { Suspense } from "solid-js";
 import { 
   Title,
   createRouteData,
   RouteDataArgs,
-  useRouteData
+  useRouteData,
+  useRoutes,
+  Meta
 } from "solid-start";
 import Counter from "~/components/Counter";
 
-// export function routeData({ params } : RouteDataArgs) {
-//   return createRouteData(
-//     async key => {
-//       const storeInfoResp = await fetch(
-//         `https://commerce.teespring.com/v1/stores?slug=${key[0]}`
-//       );
-//       const themeInfoResp = await fetch(
-//         `https://kxqd7cf966.execute-api.us-west-1.amazonaws.com/dev/themes?siteKey=${key[0]}`
-//       );
-//       const storeInfo = await storeInfoResp.json();
-//       const themeInfo = await themeInfoResp.json();
-//       return ({
-//         name: storeInfo.name,
-//         slug: key[0],
-//         logo: themeInfo.content.header.logo.replace(
-//           '{{assetPath}}',
-//           `//premium-storefronts.s3.amazonaws.com/storefronts/${key[0]}/assets`
-//         ),
-//         banner: themeInfo.content.heroBanner.containerBg.replace(
-//           '{{assetPath}}',
-//           `//premium-storefronts.s3.amazonaws.com/storefronts/${key[0]}/assets`
-//         ),
-//       });
-//     },
-//     { key: () => ["browniebits"] }
-//   );
-// }
+export function routeData({ params } : RouteDataArgs) {
+  return createRouteData(
+    async key => {
+      const storeInfoResp = await fetch(
+        `https://commerce.teespring.com/v1/stores?slug=${key[0]}`
+      );
+      const themeInfoResp = await fetch(
+        `https://kxqd7cf966.execute-api.us-west-1.amazonaws.com/dev/themes?siteKey=${key[0]}`
+      );
+      const storeInfo = await storeInfoResp.json();
+      const themeInfo = await themeInfoResp.json();
+      return ({
+        name: storeInfo.name,
+        slug: key[0],
+        logo: themeInfo.content.header.logo.replace(
+          '{{assetPath}}',
+          `//premium-storefronts.s3.amazonaws.com/storefronts/${key[0]}/assets`
+        ),
+        banner: themeInfo.content.heroBanner.containerBg.replace(
+          '{{assetPath}}',
+          `//premium-storefronts.s3.amazonaws.com/storefronts/${key[0]}/assets`
+        ),
+      });
+    },
+    { key: () => ["browniebits"] }
+  );
+}
 
 export default function Home() {
-  // const storeInfo = useRouteData<typeof routeData>();
-  
+  const storeInfo = useRouteData<typeof routeData>();
+  console.log('STORE INFO', storeInfo()?.name)
   return (
-    <main>
-      <Title>Boop</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+    <Suspense fallback={<span>Loading Info...</span>}>
+      <main>
+        <Title>{storeInfo()?.name}</Title>
+        <Meta property="og:title" content={storeInfo()?.name} />
+        <h1>Hello world!</h1>
+        <Counter />
+        <p>
+          Visit{" "}
+          <a href="https://start.solidjs.com" target="_blank">
+            start.solidjs.com
+          </a>{" "}
+          to learn how to build SolidStart apps.
+        </p>
+      </main>
+    </Suspense>
   );
 }
