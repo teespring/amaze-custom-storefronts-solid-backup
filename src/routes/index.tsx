078b23099d38723d8a69a663d5173f6511398319
Ian Brown <ian.brown@spri.ng>
2023-02-20 20:51:35 -0700

    added banner

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,7 @@
-import { Suspense,Show, createResource } from "solid-js";
-import { 
-  Title,
-  Meta,
-  Link
-} from "solid-start";
-import { useBrowserLocation } from 'solidjs-use'
-import FixAssetPathUrl from "~/components/helpers/FixAssetPathUrl";
+import { Suspense, Show, createResource } from 'solid-js';
+import { Title, Meta, Link } from 'solid-start';
+import { useBrowserLocation } from 'solidjs-use';
+import FixAssetPathUrl from '~/components/helpers/FixAssetPathUrl';
 import { ThemeInfo, StoreInfo } from '../lib/typeDefs';
 
 const fetchTheme = async () =>
@@ -14,38 +10,34 @@ const fetchTheme = async () =>
       'https://kxqd7cf966.execute-api.us-west-1.amazonaws.com/dev/themes?siteKey=browniebits'
     )
   ).json();
-  const fetchStore = async () =>
+const fetchStore = async () =>
   (
-    await fetch(
-      'https://commerce.teespring.com/v1/stores?slug=browniebits'
-    )
+    await fetch('https://commerce.teespring.com/v1/stores?slug=browniebits')
   ).json();
 
 export default function Home() {
-  const location = useBrowserLocation()
-  const hrefArray = location().href?.replace(`${location().protocol}//`,'').split('.');
+  const location = useBrowserLocation();
+  const hrefArray = location()
+    .href?.replace(`${location().protocol}//`, '')
+    .split('.');
   const subDomain = hrefArray ? hrefArray[0] : 'www';
   const [theme] = createResource<ThemeInfo>(fetchTheme, { initialValue: {} });
   const [store] = createResource<StoreInfo>(fetchStore, { initialValue: {} });
-  console.log(store())
+  console.log(store());
   return (
-    <>
-      <span style="display:none">{store().name}</span>
-      <Suspense fallback={<></>}>
-        <main>
-          <Title>{store().name}</Title>
-          <Meta property="og:title" content={store().name} />
-          <Meta
-            property="og:image"
-            content={FixAssetPathUrl(theme().content?.heroBanner.containerBg!)}
-          />
-          <Link
-            rel="icon"
-            href={FixAssetPathUrl(theme().content?.heroBanner.containerBg!)}
-          />
-          <h1>Hello {store().name}!</h1>
-        </main>
-      </Suspense>
-    </>
+    <main>
+      <Title>{store().name}</Title>
+      <Meta property="og:title" content={store().name} />
+      <Meta
+        property="og:image"
+        content={FixAssetPathUrl(theme().content?.heroBanner.containerBg!)}
+      />
+      <Link rel="icon" href={FixAssetPathUrl(theme().content?.favicon!)} />
+      <Show when={theme().content?.heroBanner.containerBg} fallback={<></>}>
+        <div class="hero">
+          <img src={FixAssetPathUrl(theme().content?.heroBanner.containerBg!)}/>
+        </div>
+      </Show>
+    </main>
   );
 }
