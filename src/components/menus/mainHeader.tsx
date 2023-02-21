cc8663c95fb5747a532aa03af43a0910e5f509ed
Ian Brown <ian.brown@spri.ng>
2023-02-20 18:39:27 -0700

    trying without id

--- src/components/menus/mainHeader.tsx
+++ src/components/menus/mainHeader.tsx
@@ -1,58 +1,49 @@
 import styles from './mainHeader.module.scss';
 import { Show, For, Suspense, createResource } from "solid-js";
 import { A, useLocation } from "solid-start";
-// import { theme } from '../../lib/store';
+import { theme } from '../../lib/store';
 
-
-// export default function MainHeader ( ) {
-//     const location = useLocation();
-//     return (
-//         <>
-//         <span>{theme.loading && "Loading..."}</span>
-//         <Suspense>
-//             <Show 
-//             when={location.pathname != '/checkout'}
-//             fallback={<></>}
-//             >
-                
-//                 <header class={styles.header} style={`background:#000000`}>
-//                     <A href="/">
-//                         <div class={styles.logo}>
-//                             <h1 style={`color:${[theme()?.styles.header.textStyles.color]}`}>Brownie Bits</h1>
-//                             {/* <Show 
-//                             when={themeInfo().content.header.logo != ''}
-//                             fallback={<h1 style={`color:${[themeInfo().styles.header.textStyles.color]}`}>Brownie Bits</h1>}>
-//                                 <img src={themeInfo().content.header.logo} height={themeInfo().styles.header.logo.maxHeight}/>
-//                             </Show> */}
-//                         </div>
-//                     </A>
-//                     <nav class={styles.nav}>
-                        
-//                     {/* <For each={props.collections}>
-//                         {collection => {
-//                             return <li>
-//                             <A href={`/${collection.name}`}>
-//                             {collection.name}
-//                             </A>
-//                             </li>
-//                         }}
-//                     </For> */}
-//                     </nav>
-//                 </header>
-//             </Show>
-//         </Suspense>
-//         </>
-//     )
-// }
-
-const fetchJokes = async () => (await fetch('https://official-joke-api.appspot.com/jokes/programming/ten')).json();
 
 export default function MainHeader ( ) {
-    const [jokes] = createResource(fetchJokes);
+    const location = useLocation();
     return (
         <>
-        <span>{jokes.loading && "Loading..."}</span>
-        <pre>{JSON.stringify(jokes(), null, 2)}</pre>
+        <span>{theme.loading && "Loading..."}</span>
+        <Suspense>
+            <Show 
+            when={location.pathname != '/checkout'}
+            fallback={<></>}
+            >
+                
+                <header class={styles.header} style={`background:#000000`}>
+                    <A href="/">
+                        <div class={styles.logo}>
+                            <h1 style={`color:${[theme()?.styles.header.textStyles.color]}`}>Brownie Bits</h1>
+                            {/* <Show 
+                            when={themeInfo().content.header.logo != ''}
+                            fallback={<h1 style={`color:${[themeInfo().styles.header.textStyles.color]}`}>Brownie Bits</h1>}>
+                                <img src={themeInfo().content.header.logo} height={themeInfo().styles.header.logo.maxHeight}/>
+                            </Show> */}
+                        </div>
+                    </A>
+                    <nav class={styles.nav}>
+                        
+                    {/* <For each={props.collections}>
+                        {collection => {
+                            return <li>
+                            <A href={`/${collection.name}`}>
+                            {collection.name}
+                            </A>
+                            </li>
+                        }}
+                    </For> */}
+                    </nav>
+                </header>
+            </Show>
+        </Suspense>
         </>
     )
 }
+
+
+
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,27 +1,9 @@
 import { createResource } from 'solid-js';
 import { ThemeInfo } from './typeDefs';
 
-const fetchTheme = async () => {
-  const themeResp = await fetch(
-    `https://kxqd7cf966.execute-api.us-west-1.amazonaws.com/dev/themes?siteKey=browniebits`
-  );
-  const themeData: ThemeInfo = await themeResp.json();
-  themeData.content.header.logo = themeData.content.header.logo.replace(
-    '{{assetPath}}',
-    `//premium-storefronts.s3.amazonaws.com/storefronts/browniebits/assets`
-  );
-  themeData.content.favicon = themeData.content.favicon.replace(
-    '{{assetPath}}',
-    `//premium-storefronts.s3.amazonaws.com/storefronts/browniebits/assets`
-  );
-  themeData.content.heroBanner.containerBg =
-    themeData.content.heroBanner.containerBg.replace(
-      '{{assetPath}}',
-      `//premium-storefronts.s3.amazonaws.com/storefronts/browniebits/assets`
-    );
-    themeData.id = 'browniebits';
-    console.log(themeData)
-    return themeData
-};
-
+const fetchTheme = async () => (await fetch('https://kxqd7cf966.execute-api.us-west-1.amazonaws.com/dev/themes?siteKey=browniebits')).json();
 export const [theme] = createResource<ThemeInfo>(fetchTheme);
+
+
+const fetchJokes = async () => (await fetch('https://official-joke-api.appspot.com/jokes/programming/ten')).json();
+export const [jokes] = createResource(fetchJokes);
